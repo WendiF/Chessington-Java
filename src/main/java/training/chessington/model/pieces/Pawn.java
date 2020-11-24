6243d9f5f6e658fa6885d29e3f72b3d4e9709450
WendiF <wendi.fan@softwire.com>
2020-11-24 12:26:33 +0000

    Pawns don't move off end of board

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -12,20 +12,30 @@ public class Pawn extends AbstractPiece {
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
     }
+    private int BOARD_SIZE = 7;
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> moves =  new ArrayList<>();
+
         int direction = board.get(from).getColour().equals(PlayerColour.WHITE) ? -1  : 1;
-        
-        if (Math.floorMod(direction, 7) == from.getRow()) {
+
+        if (Math.floorMod(direction, BOARD_SIZE) == from.getRow()) {
             moves.add(new Move(from, from.plus(direction*2, 0)));
         }
 
-        if (board.get(from.plus(direction, 0)) == null) {
+        if (!isAtEndOfBoard(from, direction) && notBlocked(from, board, direction)) {
             moves.add(new Move(from, from.plus(direction, 0)));
-
         }
+
         return moves;
+    }
+
+    private boolean isAtEndOfBoard(Coordinates from, int direction) {
+        return (direction == -1 & from.getRow() == 0) || (direction == 1 & from.getRow() == 7);
+    }
+
+    private boolean notBlocked(Coordinates from, Board board, int direction) {
+        return board.get(from.plus(direction, 0)) == null;
     }
 }
